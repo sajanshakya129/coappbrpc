97a6e8e29f6a3550cbd0c70e96c9d7967a7813b1
Sajan <ersajanshakya129@gmail.com>
2018-09-24 15:29:21 +0200

    client commit changes

--- src/compile_client.sh
+++ src/compile_client.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 echo compiling client for testing
-c++ -o client ../proto/rpc_ping.pb.cc ../deps/libpbrpc/proto/pbrpc.pb.cc ClientRPC.cc coap_common.cc coap_client.cc client.cpp `pkg-config --cflags --libs protobuf` -lcurl -I/usr/local/include/pbrpc -lcoap-2
+c++ -o client ../proto/rpc_ping.pb.cc ../build/deps/libpbrpc/proto/pbrpc.pb.cc ClientRPC.cc coap_common.cc coap_client.cc client.cpp `pkg-config --cflags --libs protobuf` -lcurl -I/usr/local/include/pbrpc -lcoap-2
 
