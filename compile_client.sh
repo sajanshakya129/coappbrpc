0a7133f58c3102bb6c57ac70b11366a25ab75d00
Sajan <ersajanshakya129@gmail.com>
2018-09-26 16:05:53 +0200

    http codes removed

--- compile_client.sh
+++ compile_client.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 echo compiling client for testing
-c++ -o client build/proto/rpc_ping.pb.cc build/deps/libpbrpc/proto/pbrpc.pb.cc src/ClientRPC.cc src/coap_common.cc src/coap_client.cc src/client.cpp `pkg-config --cflags --libs protobuf` -lcurl -I/usr/local/include/pbrpc -lcoap-2
+c++ -o client build/proto/rpc_ping.pb.cc build/deps/libpbrpc/proto/pbrpc.pb.cc src/ClientRPC.cc src/coap_common.cc src/coap_client.cc src/client.cpp `pkg-config --cflags --libs protobuf` -I/usr/local/include/pbrpc -lcoap-2
 
--- run.sh
+++
@@ -1,3 +0,0 @@
-echo'compiling client for testing'
-c++ -o client ../proto/rpc_ping.pb.cc ../deps/libpbrpc/proto/pbrpc.pb.cc client.cpp `pkg-config --cflags --libs protobuf` -lcurl -I/usr/local/include/pbrpc
-
