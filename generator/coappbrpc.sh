39ef0ba7f276b5a728a2d684e698ec95542b7a4c
Sajan <ersajanshakya129@gmail.com>
2018-10-16 17:15:06 +0200

    updated

--- generator/coappbrpc.sh
+++ generator/coappbrpc.sh
@@ -4,14 +4,19 @@ if [ -z $1 ]
   then
     echo "Syntax Error: Command must be in following format \"coappbrpc <file.proto>\""
   else
-  	if [[ ${1: -6} = ".proto" ]]
+  	if [[ ${1: -6} = ".proto" ]] 
   		then
-  			echo "Creating Stub for $1"
-			protoc --plugin=protoc-gen-custom=/usr/local/lib/coappbrpc/templates/coappbrpcPlugin.py --custom_out=./ $1
-			echo "Creating h and cc files from $1"
-			protoc  --cpp_out=./ $1
-			echo "Creating Client stub file"
-			python -m cog -d  @/usr/local/lib/coappbrpc/templates/cogFiles.txt
+  			if [ -e $1 ]
+  			then
+	  			echo "Creating Stub for $1"
+				protoc --plugin=protoc-gen-custom=/usr/local/lib/coappbrpc/templates/coappbrpcPlugin.py --custom_out=./ $1
+				echo "Creating h and cc files from $1"
+				protoc  --cpp_out=./ $1
+				echo "Creating Client stub file"
+				python -m cog -d  @/usr/local/lib/coappbrpc/templates/cogFiles.txt
+			else
+				echo "File doesnot exists."
+			fi
  		else
  			echo "File must be .proto files."
 	fi
