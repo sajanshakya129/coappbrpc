e1eecfce11f6d8e79cdef3fc48d667061906ad4d
Sajan <ersajanshakya129@gmail.com>
2018-11-08 10:23:18 +0100

    comments added

--- generator/templates/ClientStubTemplate.cc
+++ generator/templates/ClientStubTemplate.cc
@@ -22,8 +22,7 @@ with open('protofile.json') as fp:
 for item in jsonData["data"]:
         if item["type"]=="Service":
                 for method in item["methods"]:
-                        cog.outl("void ClientStub::{0}({1} request,{2}
-*response)".format(method["method_name"], method["input"], method["output"]))
+                        cog.outl("void ClientStub::{0}({1} request,{2}*response)".format(method["method_name"], method["input"], method["output"]))
                         cog.outl("{ Response resp;")
                         cog.outl("resp=client->execFunc(\"1.1\",\"{0}\",\"{1}\",serializeMsg(request));".format(item["name"],method["method_name"]))
                         cog.outl("response->ParseFromString(resp.result());")
--- src/RpcServer.cc
+++ src/RpcServer.cc
@@ -16,7 +16,26 @@
 */
 #define UNUSED_PARAM
 namespace coappbrpc {
+/*! \fn void return_handler(coap_context_t *ctx,
+                    struct coap_resource_t *resource,
+                    coap_session_t *session,
+                    coap_pdu_t *request,
+                    coap_binary_t *token,
+                    coap_string_t *query, coap_pdu_t *response)
 
+    \brief Callback function that sets response data to be sent back to RPC
+   client
+   \param ctx Coap context variable reference of type struct
+   coap_context_t.
+   \param resource Coap resource referece.
+   \param session Coap Session variable reference of type
+   coap_session_t
+   \param request CoAP PDU request received by server
+   \param token CoAP unique token references
+   \param query CoAP query reference
+   \param response CoAP PDU response reference for storing data to be returned
+   from Server
+*/
 void return_handler(coap_context_t *ctx UNUSED_PARAM,
                     struct coap_resource_t *resource UNUSED_PARAM,
                     coap_session_t *session UNUSED_PARAM,
@@ -44,25 +63,39 @@ void return_handler(coap_context_t *ctx UNUSED_PARAM,
                 (const uint8_t *)response_data);
 }
 
-RpcServer::RpcServer() {
-  running = false;
-  //initService();
-}
 
+/*! \fn RpcServer::RpcServer()
+    \brief Constructor function which sets variable "running" to false for every instance 
+*/
+RpcServer::RpcServer() { running = false; }
+
+/*! \fn RpcServer::_RpcServer()
+    \brief Destructor Function
+*/
 RpcServer::~RpcServer() {}
 
+/*! \fn void RpcServer::runServer(const char *ipAddr, const char *port)
+    \brief Setter function to set server ip address and port number and calls for "start" function to run Server.
+      Default address is localhost and port is 5683. Function overloading used for handling default values incase parameters are not passed.
+    \param ipAddr Ip Address reference variable
+    \param port port reference variable
+*/
 void RpcServer::runServer(const char *ipAddr, const char *port) {
-  this->serverAddr=ipAddr;
-  this->port=port;
+  this->serverAddr = ipAddr;
+  this->port = port;
   this->start();
 }
-
+/*! \fn RpcServer::RpcServer()
+    \brief Getter function to get response stored in m_response variable
+*/
 void RpcServer::runServer() {
   this->serverAddr = "localhost";
   this->port = "5683";
   this->start();
 }
-
+/*! \fn RpcServer::start()
+    \brief creates context 
+*/
 int RpcServer::start() {
   if (running) {
     return 0;
@@ -77,14 +110,14 @@ int RpcServer::start() {
   coap_str_const_t ruri = {3, (const uint8_t *)COAP_INTERFACE_NAME};
 
   coap_startup();
-  std::cout<<this->serverAddr<<std::endl;
+  std::cout << this->serverAddr << std::endl;
   /* resolve destination address where server should be sent */
   if (common.resolveAddress(this->serverAddr, this->port, &dst) < 0) {
     coap_log(LOG_CRIT, "failed to resolve address\n");
     this->stop(EXIT_FAILURE);
   }
 
-  /* create CoAP context and a client session */
+  /* create CoAP context */
   ctx = this->ctx;
   ctx = coap_new_context(nullptr);
 
@@ -93,18 +126,16 @@ int RpcServer::start() {
     this->stop(EXIT_FAILURE);
   }
 
-  // This code is automatically generated.
   resource = coap_resource_init(&ruri, 0);
   coap_register_handler(resource, COAP_REQUEST_POST, return_handler);
   coap_add_resource(ctx, resource);
-  
+
   while (true) {
     coap_run_once(ctx, 0);
   }
   running = true;
   this->stop(EXIT_SUCCESS);
 }
-
 
 bool RpcServer::stop(int result) {
   if (running) {
@@ -116,8 +147,6 @@ bool RpcServer::stop(int result) {
   return !running;
 }
 
-void RpcServer::registerService(Service *service) {
-  handleRegService(service);
-}
+void RpcServer::registerService(Service *service) { handleRegService(service); }
 
 } // namespace coappbrpc
